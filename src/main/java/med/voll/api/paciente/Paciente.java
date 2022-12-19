d585b866bea250f81516c706e9b6dfb3ba4f215e
Caroline Ferraz <projetos.crrz@gmail.com>
2022-12-19 11:51:46 -0300

    Feat: Criando entidade Paciente, DTO DadosCadastroPaciente, interface PacienteRepository e controller PacienteController

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,30 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import med.voll.api.endereco.Endereco;
+
+public class Paciente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String cpf;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	public Paciente(DadosCadastroPaciente dados) {
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.cpf = dados.cpf();
+		this.endereco = new Endereco(dados.endereco());
+	}
+	
+}
