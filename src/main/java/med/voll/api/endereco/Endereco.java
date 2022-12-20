d3c1af111e0c4d0e91a8bb985530f16b2edf05e1
Caroline Ferraz <projetos.crrz@gmail.com>
2022-12-20 16:13:45 -0300

    Feat: Criando método para atualização de médico na classe MedicoController e, para isso, criando também o DTO DadosAtualizacaoMedico e os métodos atualizarInformacoes() na entidade Medico e na classe Endereco

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -33,4 +33,31 @@ public class Endereco {
 		super();
 	}
 
+	public void atualizarInformacoes(DadosEndereco dados) {
+		if(dados.logradouro() != null) {
+			this.logradouro = dados.logradouro();
+		}
+		if(dados.numero() != null) {
+			this.numero = dados.numero();
+		}
+		if(dados.complemento() != null) {
+			this.complemento = dados.complemento();
+		}
+		if(dados.cep() != null) {
+			this.cep = dados.cep();
+		}
+		if(dados.bairro() != null) {
+			this.bairro = dados.bairro();
+		}
+		if(dados.cidade() != null) {
+			this.cidade = dados.cidade();
+		}
+		if(dados.uf() != null) {
+			this.uf = dados.uf();
+		}
+		
+	}
+	
+	
+
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -47,6 +48,18 @@ public class Medico {
 		this.crm = dados.crm();
 		this.especialidade = dados.especialidade();
 		this.endereco = new Endereco(dados.endereco());
+	}
+	
+	public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+		if(dados.nome() != null) {
+			this.nome = dados.nome();
+		}
+		if(dados.telefone() != null) {
+			this.telefone = dados.telefone();
+		}
+		if(dados.endereco() != null) {
+			this.endereco.atualizarInformacoes(dados.endereco());
+		}
 	}
 
 	public Long getId() {
@@ -104,6 +117,5 @@ public class Medico {
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-	
 	
 }
