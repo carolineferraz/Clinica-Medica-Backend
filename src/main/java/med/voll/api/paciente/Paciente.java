75eda30135f3ac614fb8b0b8330cf48f0618f0ad
Caroline Ferraz <projetos.crrz@gmail.com>
2022-12-20 12:46:59 -0300

    Feat: Criando método de listagem de pacientes na classe PacienteController e, para isso, criando também o DTO DadosListagemPaciente. Método aplicado com as interface Page e Pageable do Spring, para aplicar a paginação padrão

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -38,5 +38,57 @@ public class Paciente {
 		this.cpf = dados.cpf();
 		this.endereco = new Endereco(dados.endereco());
 	}
+
+	public Paciente() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 	
 }
