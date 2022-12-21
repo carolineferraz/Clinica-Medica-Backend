ed4407b28d57948da5fb0fa9d71059aca9bf200b
Caroline Ferraz <projetos.crrz@gmail.com>
2022-12-21 15:17:43 -0300

    Feat: Criando método para exclusão de paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +44,12 @@ public class PacienteController {
 	public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
 		var paciente = repository.getReferenceById(dados.id());
 		paciente.atualizarInformacoes(dados);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 
 }
