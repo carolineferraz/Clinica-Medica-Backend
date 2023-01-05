9f44f1b506273905f648c7f072cc58277e7323b4
Caroline Ferraz <projetos.crrz@gmail.com>
2023-01-04 22:54:02 -0300

    Feat: Adicionando método GET por Id para detalhar os dados de um paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -65,5 +65,12 @@ public class PacienteController {
 		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity detalhar(@PathVariable Long id) {
+		var paciente = repository.getReferenceById(id);
+		
+		return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
+	}
 
 }
