abf3b1707d0663350e19908b26ba90a8d7629f40
Caroline Ferraz <projetos.crrz@gmail.com>
2023-07-01 14:08:09 -0300

    Fix: Ajustando os query methods das interfaces MedicoRepostory e ConsultaRepository para resolver efeito colateral no agendamento de consultas devido ao cancelamento de consultas

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -11,21 +11,23 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
 	Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
-	@Query("""  
-			select m from Medico m
-			where 
-			m.ativo = 1
+	@Query("""
+            select m from Medico m
+            where
+            m.ativo = 1
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.data = :data
 			and
-			m.especialidade = :especialidade
-			and
-			m.id not in(
-				select c.medico.id from Consulta c
-				where 
-				c.data = :data
-			)
-			order by rand()
-			limit 1
-			""")
+                c.motivoCancelamento is null
+            )
+            order by rand()
+            limit 1
+	""")
 	Medico escolherMedicoAleatorioPorEspecialidadeEDisponibilidade(Especialidade especialidade, LocalDateTime data);
 
 	@Query("""
